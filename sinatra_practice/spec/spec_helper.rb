70a7f244343f956bf04df55eaaf63999680ccb0c
Ayana <ayanaflax@yahoo.com>
2018-09-26 18:48:09 -0400

    add test folders and update gems

---
+++ sinatra_practice/spec/spec_helper.rb
@@ -0,0 +1,17 @@
+
+ENV['RACK_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+require 'rspec'
+require 'capybara/dsl'
+require 'database_cleaner'
+Capybara.app = SinatraPracticeApp
+DatabaseCleaner.strategy = :truncation
+RSpec.configure do |c|
+  c.include Capybara::DSL
+c.before(:all) do
+    DatabaseCleaner.clean
+  end
+c.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
