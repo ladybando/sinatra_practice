45b72d73064aa718b5e9d616ee5e0ac5f3be0d7a
Ayana <ayanaflax@yahoo.com>
2018-09-28 14:31:08 -0400

    complete basic blog app code

--- sinatra_practice/app/controllers/sinatra_practice_app.rb
+++ sinatra_practice/app/controllers/sinatra_practice_app.rb
@@ -1,2 +1,20 @@
 class SinatraPracticeApp < Sinatra::Base
+    get ‘/’ do
+        erb :”blogs/new”
+    end
+
+    post ‘/’ do
+        blog = Blog.new(params[“blog”])
+        if blog.save
+          redirect “/blogs/#{blog.id}”
+        else
+          erb :”blogs/new”
+        end       
+    end
+
+    get ‘/blogs/:id’ do
+        @blog = Blog.find(params[:id])
+        erb :”blogs/show”
+      end
+    end
 end
--- sinatra_practice/config/environment.rb
+++ sinatra_practice/config/environment.rb
@@ -1,4 +1,4 @@
-require 'pry'
+
 require ‘bundler’
 Bundler.require
 
